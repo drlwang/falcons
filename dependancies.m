14ef66762131ea2c5328420b7c36ea4dbb46c2ec
Lin Wang <git@linwang.xyz>
2022-02-04 23:22:38 +0000

    move from GitLab to Github for public usage

---
+++ dependancies.m
@@ -0,0 +1,7 @@
+% this file documents the depandencies of the FALCON tool
+% 3d party toolbox: export_fig.m ; if no figure is saved then no need to
+% download this toolbox, else refer to the link https://github.com/altmany/export_fig 
+% for more detail and installation
+
+% Update: L. Wang, BKG, 2022-01-02
+%           add the documents of the dependencies
